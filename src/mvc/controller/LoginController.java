966d495440b39d842697f605abfbcfd1255e6b7d
slimkaki <rafaalmada98@gmail.com>
2019-09-19 09:01:43 -0300

    update LoginControl

--- src/mvc/controller/LoginController.java
+++ src/mvc/controller/LoginController.java
@@ -1,5 +1,12 @@
 package mvc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class LoginController {
+	
+	@RequestMapping("/login")
+	public String efetuaLogin() {
+		return "login";
+	}
 
 }
